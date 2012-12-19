ac9e1236a75a2fc79caac096e17ff5d05363990b
Ryan Grove <ryan@wonko.com>
2012-12-19 13:57:39 -0800

    Copy built files into gh-pages.

--- regen.sh
+++ regen.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
-# Run this script to regenerate API docs. It assumes you have the SmugMug
-# yui-gallery repo checked out at ../yui-gallery.
+# Run this script to copy built files into this repo and regenerate API docs. It
+# assumes you have the master branch of the yui-gallery repo checked out at
+# ../yui-gallery.
 #
 
 DOC_ROOT=$PWD
+
+rm -rf ./build
+cp -R ../yui-gallery/build ./
 
 cd ../yui-gallery
 yuidoc -c "$DOC_ROOT"/yuidoc.json -o "$DOC_ROOT"/api src
