2a8d90d08c7968483f8f25f37e8a0470d3c4b062
Ryan Grove <ryan@wonko.com>
2012-12-18 17:40:11 -0800

    Regen docs.

--- api/api.js
+++ api/api.js
@@ -1,18 +1,53 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Menu",
+        "Menu.Base",
+        "Menu.Item",
+        "Menu.Templates",
+        "Plugin.Menu",
         "Plugin.Tree.Lazy",
         "Tree",
         "Tree.Node",
         "TreeView"
     ],
     "modules": [
+        "gallery-sm-menu",
+        "gallery-sm-menu-base",
+        "gallery-sm-menu-item",
+        "gallery-sm-menu-plugin",
+        "gallery-sm-menu-templates",
         "gallery-sm-tree",
         "gallery-sm-tree-lazy",
         "gallery-sm-tree-node",
         "gallery-sm-treeview"
     ],
     "allModules": [
+        {
+            "displayName": "gallery-sm-menu",
+            "name": "gallery-sm-menu",
+            "description": "Provides the `Y.Menu` widget."
+        },
+        {
+            "displayName": "gallery-sm-menu-base",
+            "name": "gallery-sm-menu-base",
+            "description": "Provides `Menu.Base`."
+        },
+        {
+            "displayName": "gallery-sm-menu-item",
+            "name": "gallery-sm-menu-item",
+            "description": "Provides the `Menu.Item` class."
+        },
+        {
+            "displayName": "gallery-sm-menu-plugin",
+            "name": "gallery-sm-menu-plugin",
+            "description": "Provides the `Y.Plugin.Menu` Node plugin."
+        },
+        {
+            "displayName": "gallery-sm-menu-templates",
+            "name": "gallery-sm-menu-templates",
+            "description": "Provides templates for `Menu`."
+        },
         {
             "displayName": "gallery-sm-tree",
             "name": "gallery-sm-tree",
