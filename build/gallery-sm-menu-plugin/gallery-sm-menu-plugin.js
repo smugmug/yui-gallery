1d10cfc478cd92d439cb747e214b45b048600cc7
Ryan Grove <ryan@wonko.com>
2013-01-04 10:47:12 -0800

    Build gallery-sm-menu.

--- build/gallery-sm-menu-plugin/gallery-sm-menu-plugin.js
+++ build/gallery-sm-menu-plugin/gallery-sm-menu-plugin.js
@@ -113,9 +113,9 @@ Y.namespace('Plugin').Menu = Y.Base.create('menuPlugin', Y.Menu, [Y.Plugin.Base]
         var container = this.get('container'),
             host      = this._host;
 
-        return (function (node) {
+        return function (node) {
             return node === container || node === host;
-        });
+        };
     },
 
     // -- Protected Event Handlers ---------------------------------------------
