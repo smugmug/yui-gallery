f22c78f74f60ce2603abb750e9777a1431aac6be
Ryan Grove <ryan@wonko.com>
2013-02-05 11:31:04 -0800

    Fire focus and blur events. Closes #5

--- src/sm-editor/js/editor-base.js
+++ src/sm-editor/js/editor-base.js
@@ -22,6 +22,20 @@ var doc          = Y.config.doc,
     getClassName = Y.ClassNameManager.getClassName;
 
 /**
+Fired after this editor loses focus.
+
+@event blur
+**/
+var EVT_BLUR = 'blur';
+
+/**
+Fired after this editor receives focus.
+
+@event focus
+**/
+var EVT_FOCUS = 'focus';
+
+/**
 Fired after this editor is rendered.
 
 @event render
@@ -562,6 +576,8 @@ var EditorBase = Y.Base.create('editorBase', Y.View, [], {
 
         clearInterval(this._selectionMonitor);
         this._updateSelection();
+
+        this.fire(EVT_BLUR);
     },
 
     /**
@@ -584,6 +600,8 @@ var EditorBase = Y.Base.create('editorBase', Y.View, [], {
         this._selectionMonitor = setInterval(function () {
             self._updateSelection();
         }, 200);
+
+        this.fire(EVT_FOCUS);
     },
 
     /**
