a53f108420abf679de97c9453ee021c9be21eba0
Lee Shepherd <bigwebguy@gmail.com>
2013-07-22 12:38:36 -0400

    editor-dom: added split method, getAncestorElement now takes a function as the second argument

--- src/sm-editor/js/extensions/editor-dom.js
+++ src/sm-editor/js/extensions/editor-dom.js
@@ -16,45 +16,94 @@ DOM utility methods for `Editor.Base`
 var EditorDOM = function() {};
 
 /**
-Finds the nearest ancestor with a given tagName
+Finds the nearest ancestor element node.
 
 @param {Node} node
-@param {String} [tagName] Optional tagName. If not provided, the nearest
-  ancestor element node will be returned
+@param {Function|String} [tagName] Optional tagName or function. If a tagName
+  is provided, the nearest ancestor with that tag will be returned. If a
+  function is provided, it will be used for the comparision. It will receive
+  a node as its only argument and should return a boolean. If nothing is
+  provided, the nearest ancestor element node will be returned
 @return {Node}
 @static
 **/
 EditorDOM.getAncestorElement = function(node, tagName) {
-    tagName && (tagName = tagName.toUpperCase());
+    var fn;
 
     function tagFn(node) {
         var nodeTag = node.get('tagName');
-
         return nodeTag && tagName === nodeTag.toUpperCase();
     }
 
-    return node.ancestor(tagName ? tagFn : EditorDOM.isElementNode, true);
+    if ('function' === typeof tagName) {
+        fn = tagName;
+    } else if ('string' === typeof tagName) {
+        tagName = tagName.toUpperCase();
+        fn = tagFn;
+    }
+
+    return node.ancestor(fn || EditorDOM.isElementNode, true);
 };
 
 
 /**
+Returns true if the given node is a block element, false otherwise
+
+@method isBlockElement
+@param {HTMLElement|Node} node
+@return {Boolean} true if the given node is a block element, false otherwise
+@static
+**/
+EditorDOM.isBlockElement = function(node) {
+    node = Y.one(node);
+
+    return EditorDOM.isElementNode(node) &&
+        'block' === node.getStyle('display');
+};
+
+
+/**
+Returns true if the given node is an element node, false otherwise
+
 @method isElementNode
-@param {Node} node
-@return {Boolean}
+@param {HTMLElement|Node} node
+@return {Boolean} true if the given node is an element node, false otherwise
 @static
 **/
 EditorDOM.isElementNode = function(node) {
+    node = Y.one(node);
+
     return 1 === node.get('nodeType');
 };
 
 
 /**
+Returns true if the given node is an inline element node, false otherwise
+
+@method isInlineElement
+@param {HTMLElement|Node} node
+@return {Boolean} true if the given node is an inline element node, false otherwise
+@static
+**/
+EditorDOM.isInlineElement = function(node) {
+    node = Y.one(node);
+
+    return EditorDOM.isElementNode(node) &&
+        /^inline/.test(node.getStyle('display'));
+};
+
+
+/**
+Returns true if the given node is a text node, false otherwise
+
 @method isTextNode
-@param {Node} node
-@return {Boolean}
+@param {HTMLElement|Node} node
+@return {Boolean} true if the given node is a text node, false otherwise
 @static
 **/
 EditorDOM.isTextNode = function(node) {
+    node = Y.one(node);
+
     return 3 === node.get('nodeType');
 };
 
@@ -78,6 +127,45 @@ EditorDOM.maxOffset = function(node) {
 
 
 /**
+Splits the given node at the specified offset
+
+@method split
+@param {Node} node The node to split
+@param {Number|Node} offset Position to split the node.
+    For text nodes this is numerical offset, for element nodes can be a
+    a number referencing a childNode of `node` or a childNode reference.
+@returns {Node} Node reference to the node created after splitting. It will
+    be the nextSibling of `node`. If offset is not valid, the original `node`
+    is returned.
+**/
+EditorDOM.split = function(node, offset) {
+    var childNodes = node.get('childNodes'),
+        splitNode;
+
+    node = Y.one(node);
+
+    if (offset && offset._node) {
+        offset = childNodes.indexOf(offset);
+    } else {
+        offset = parseInt(offset, 10);
+    }
+
+    if (isNaN(offset) || offset < 0 || offset > EditorDOM.maxOffset(node)) {
+        return node;
+    }
+
+    if (EditorDOM.isTextNode(node)) {
+        return Y.one(node._node.splitText(offset));
+    } else {
+        splitNode = node.cloneNode(false).append(childNodes.splice(offset));
+        node.insert(splitNode, 'after');
+
+        return splitNode;
+    }
+};
+
+
+/**
 Unwraps a node
 
 @method unwrap
@@ -86,21 +174,12 @@ Unwraps a node
 @static
 **/
 EditorDOM.unwrap = function (node) {
-    var parent, child, range, startNode, endNode;
+    var range, startNode, endNode;
 
-    parent = node.get('parentElement');
+    startNode = node.get('firstChild');
+    endNode = node.get('lastChild');
 
-    while (child = node.get('firstChild')) {
-        parent.insertBefore(child, node);
-
-        if (!startNode) {
-            startNode = child;
-        }
-
-        endNode = child;
-    }
-
-    node.remove(true);
+    startNode.unwrap();
 
     range = new Y.Range();
     range.startNode(startNode);
